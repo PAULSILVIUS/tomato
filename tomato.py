1d64c64cfb2863a6ee2db242be2df28fc6588b96
PAULSILVIUS <paulsilvius0@gmail.com>
2024-03-18 18:08:03 -0400

    built array to store time keeping

--- tomato.py
+++ tomato.py
@@ -1,5 +1,6 @@
 from datetime import datetime, timedelta
 
+
 # input
 now = datetime.now()
 print(now)
@@ -22,47 +23,68 @@ end_time = datetime(now.year, now.month, now.day, stop_hour, stop_minute, now.se
 
 # testing
 print(end_time)
-# print("Minutes leftover from 30 minutes ", work_minutes%30)
-# print("Minutes leftover from 28 minutes - end break", ((work_minutes-5)%28))
-# print("Minutes leftover from 29 minutes - end break", ((work_minutes-5)%29))
-print("Minutes leftover from 30 minutes - end break", ((work_minutes - 5) % 30))
-print("Minutes leftover from 31 minutes - end break", ((work_minutes - 5) % 31))
-print("Minutes leftover from 32 minutes - end break", ((work_minutes - 5) % 32))
-print("Minutes leftover from 33 minutes - end break", ((work_minutes - 5) % 33))
-print("Minutes leftover from 34 minutes - end break", ((work_minutes - 5) % 34))
-print("Minutes leftover from 35 minutes - end break", ((work_minutes - 5) % 35))
-# print("Minutes leftover from 36 minutes - end break", ((work_minutes-5)%36))
-print("Minutes leftover from 35 minutes ", work_minutes % 35)
+print("Minutes leftover from 30 minutes - end break", ((work_minutes + 5) % 30))
+print("Minutes leftover from 31 minutes - end break", ((work_minutes + 5) % 31))
+print("Minutes leftover from 32 minutes - end break", ((work_minutes + 5) % 32))
+print("Minutes leftover from 33 minutes - end break", ((work_minutes + 5) % 33))
+print("Minutes leftover from 34 minutes - end break", ((work_minutes + 5) % 34))
+print("Minutes leftover from 35 minutes - end break", ((work_minutes + 5) % 35))
+
 print("30 minute sections", work_minutes / 30)
-print("35 minute sections", work_minutes / 35)
+print("25 minute sections", work_minutes / 35)
 print("hours ", work_minutes // 60)
 print("minutes ", work_minutes % 60)
 
 # make method to select session splits
 # there will not be a break needed at the end for example: 25work 5break 25work 5break 25work done
 # work sessions between 25-30mins and breaks around 5 mins
-thirty_minute_leftover = (work_minutes - 5) % 30
-thirty_one_minute_leftover = (work_minutes - 5) % 31
-thirty_two_minute_leftover = (work_minutes - 5) % 32
-thirty_three_minute_leftover = (work_minutes - 5) % 33
-thirty_four_minute_leftover = (work_minutes - 5) % 34
-thirty_five_minute_leftover = (work_minutes - 5) % 35
-
-least_leftover_time = (work_minutes - 5) % 30
+least_leftover_time = (work_minutes +5) % 30
+best_section_time = 30
 for i in range(30, 35):
-    if least_leftover_time >= ((work_minutes - 5) % (i+1)):
-        least_leftover_time = (work_minutes - 5) % (i+1)
+    if least_leftover_time >= ((work_minutes + 5) % (i+1)):
+        least_leftover_time = (work_minutes + 5) % (i+1)
         best_section_time = (i+1)
 
-print(least_leftover_time)
-print(best_section_time)
+print("least leftover time: ",least_leftover_time)
+print("Best Section Time: ", best_section_time)
 
 # method that takes the time sections and puts them in to timed events
 work_section = timedelta(minutes = (best_section_time-5))
 break_section = timedelta(minutes = best_section_time)
 end_section = now + work_section
-print("work section time", end_section)
+
+print("work section time", work_section)
+print("break section time", break_section)
+print("work_minutes :", work_minutes)
+print("Work Sections :", (work_minutes - least_leftover_time + 5)/best_section_time)
+total_work_sections = (work_minutes - least_leftover_time + 5)/best_section_time
+total_break_sections = int(total_work_sections - 1)
+best_break_time = best_section_time - 5
 
 
+pomodoro_array = []
+pomodoro_array.append(0)
+temp = 0
+#add_min=least_leftover_time//total_work_sections
+#remaining_time =  least_leftover_time % total_work_sections
+#print(add_min, " ", remaining_time)
+for i in range(total_break_sections):
+    pomodoro_array.append(best_break_time+temp)
+    pomodoro_array.append(best_section_time+temp)
+    temp = best_section_time + temp
+pomodoro_array.append(work_minutes)
+
+
+print(pomodoro_array)
+# if least_leftover_time != 0:
+# if least_leftover_time > total_break_sections:
+#  least_leftover_time // total_break_sections
+
+
+# loop that takes the times and puts them into an array for break and work times
+# DO NOT FORGET TO ADD THE REMAINING TIME ONTO THE BREAKS
+
+
+# use a loop that takes in the starting time and uses wait function in time to time it out an
 
 # method that prints the times and alerts
